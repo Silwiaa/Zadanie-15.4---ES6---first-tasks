7fb5879e7fef203b02253c54d56e6cd6d4394fa0
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-06-12 20:58:32 +0200

    add 5 exc

--- script.js
+++ script.js
@@ -21,3 +21,11 @@ console.log(average(1));
 console.log(average(1, 3));
 console.log(average(1, 3, 6, 6));
 console.log(average(...grades));
+
+// EXC 5
+
+const data = [1, 4, 'Iwona', false, 'Nowak'],
+      [ , ,firstname, , ,] = data, 
+      [ , , , ,lastname] = data;
+
+console.log(firstname + ' ' + lastname);
